d9a925a169cfbd432659490f42ef58c55a274d11
Simomura0716 <simomura3109@gmail.com>
2025-04-13 20:20:53 +0900

    Update EmployeeInformation.java
    EmployeeInformation形にデータを入れた時に各要素にデータを入れれるように変更

--- Documents/src/EmployeeInformation.java
+++ Documents/src/EmployeeInformation.java
@@ -1,4 +1,4 @@
-import java.util.Calendar;
+import java.util.Date;
 
 public class EmployeeInformation {
     String employeeID;
@@ -6,8 +6,8 @@ public class EmployeeInformation {
     String firstname;
     String rubyLastName;
     String rubyFirstname;
-    Calendar birthday;
-    int joiningDate;
+    Date birthday;
+    Date joiningDate;
     int engineerDate;
     String useLanguageDate;
     String careerDate;
@@ -17,5 +17,33 @@ public class EmployeeInformation {
     Double communicationPoint;
     Double leadershipPoint;
     String remarks;
-    Calendar updatedDay;
+    Date updatedDay;
+
+    public EmployeeInformation(
+        String employeeID,
+        String lastName,String firstname,
+        String rubyLastName,String rubyFirstname,
+        Date birthday,Date joiningDate,int engineerDate,
+        String useLanguageDate,String careerDate,String trainingDate,
+        Double skillPoint,Double attitudePoint,Double communicationPoint,Double leadershipPoint,
+        String remarks,Date updatedDay
+    ){
+        this.employeeID=employeeID;
+        this.lastName=lastName;
+        this.firstname=firstname;
+        this.rubyLastName=rubyLastName;
+        this.rubyFirstname=rubyFirstname;
+        this.birthday=birthday;
+        this.joiningDate=joiningDate;
+        this.engineerDate=engineerDate;
+        this.useLanguageDate=useLanguageDate;
+        this.careerDate=careerDate;
+        this.trainingDate=trainingDate;
+        this.skillPoint=skillPoint;
+        this.attitudePoint=attitudePoint;
+        this.communicationPoint=communicationPoint;
+        this.leadershipPoint=leadershipPoint;
+        this.remarks=remarks;
+        this.updatedDay=updatedDay;
+    }
 }
