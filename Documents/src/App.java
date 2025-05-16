0cf7ff5110ee6bed478a71beaff6c4f811091dc8
ki610 <tool.kcmw@gmail.com>
2025-05-17 04:35:45 +0900

    Update App.java
    999人テスト用コード追加

--- Documents/src/App.java
+++ Documents/src/App.java
@@ -1,7 +1,10 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
+        //↓本来のコード、999人テスト時はコメントアウトしてください（木下）
         EmployeeManager manager = new EmployeeManager();
+        //↓★999人テスト用ダミーコード↓
+        //TestEmployeeManager manager = new TestEmployeeManager(); //テスト時のみ使用、999人登録コード
         manager.setUp();
     }
 }
