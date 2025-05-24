bad0fcec8fd574df08316e3288377aaf674d8561
Simomura0716 <simomura3109@gmail.com>
2025-05-24 21:00:56 +0900

    Update EmployeeUpdater.java

--- Documents/src/EmployeeUpdater.java
+++ Documents/src/EmployeeUpdater.java
@@ -224,53 +224,80 @@ public class EmployeeUpdater extends Thread {
     public void delete(ArrayList<String> selected) {
         if (LOCK.tryLock()) {
             LOCK.lock();
+            //削除処理
             try {
-                // バックアップファイル作成
-                File originalFile = new File("CSV/employee_data.csv");
-                File backupFile = new File("CSV/employee_data_backup.csv");
-                try {
-                    Files.copy(originalFile.toPath(), backupFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                    // bakkuappuログ
-                } catch (IOException e) {
-                    manager.LOGGER.info("バックアップファイルの作成に失敗しました");
-                    showErrorDialog("バックアップファイルの作成に失敗しました");
-                    return;
-                }
+                //社員情報リストから選択した社員情報を削除
                 ArrayList<EmployeeInformation> backupEmployeeList;
                 backupEmployeeList = EmployeeManager.employeeList;
-                try {
-                    synchronized (EmployeeManager.employeeList) {
+                synchronized (EmployeeManager.employeeList) {
+                    try {
                         for (Iterator<EmployeeInformation> employeeIterator = EmployeeManager.employeeList
                                 .iterator(); employeeIterator.hasNext();) {
                             EmployeeInformation employee = employeeIterator.next();
+                            // 選択された社員情報と合致したら削除
                             if (selected.contains(employee.employeeID) == true) {
                                 employeeIterator.remove();
                             }
                         }
+                    } catch (Exception e) {
+                        manager.printErrorLog(e, "選択された社員情報の削除に失敗しました");
+                        // employeeListにbackupEmployeeListをコピー
+                        EmployeeManager.employeeList.clear();
+                        EmployeeManager.employeeList = backupEmployeeList;
+                        manager.LOGGER.info("社員情報リストを削除処理前に戻しました");
+                        showErrorDialog("選択された社員情報の削除に失敗しました");
+                        return;
                     }
-                } catch (Exception e) {
-                    // employeeListにbackupEmployeeListをコピー
-                    EmployeeManager.employeeList.clear();
-                    EmployeeManager.employeeList = backupEmployeeList;
                 }
+                //社員情報保存CSVから選択した社員情報を削除
+                File originalFile = EmployeeManager.ENPLOYEE_CSV;
+                File backupFile = new File("CSV/employee_data_backup.csv");
                 try {
+                    try {
+                        // バックアップファイル作成
+                        Files.copy(originalFile.toPath(), backupFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                    } catch (IOException e) {
+                        manager.printErrorLog(e, "バックアップファイルの作成に失敗しました");
+                        showErrorDialog("バックアップファイルの作成に失敗しました");
+                        return;
+                    }
                     PrintWriter pw = new PrintWriter(new BufferedWriter(
                             new OutputStreamWriter(new FileOutputStream(originalFile), "Shift-JIS")));
+                    // 社員情報保存CSVファイルの1行目に項目名を記載
                     for (String category : EmployeeManager.EMPLOYEE_CATEGORY) {
                         pw.append(category + ",");
                     }
+                    // 行の最後で改行
                     pw.append("\n");
+                    // 社員情報リストの内容を社員情報保存CSVに上書き保存
                     for (EmployeeInformation employee : EmployeeManager.employeeList) {
                         pw.println(convertToCSV(employee));
                     }
                     pw.close();
                 } catch (Exception e) {
-                    // TODO: handle exception
+                    manager.printErrorLog(e, "削除後の社員情報リストの保存に失敗しました");
+                    if (originalFile.exists() && backupFile.exists()) {
+                        try {
+                            Files.deleteIfExists(originalFile.toPath());
+                        } catch (Exception ex) {
+                            // オリジナルの社員情報保存CSVファイルが削除に失敗した場合
+                            manager.printErrorLog(ex, "オリジナルの社員情報保存CSVファイルが削除できませんでした");
+                            showErrorDialog("オリジナルの社員情報保存CSVファイルが削除できませんでした");
+                            return;
+                        }
+                        backupFile.renameTo(originalFile);
+                    }
+                    manager.LOGGER.info("社員情報保存CSVファイルを削除処理前に戻しました");
+                    showErrorDialog("削除後の社員情報リストの保存に失敗しました");
+                    return;
                 }
                 Files.deleteIfExists(backupFile.toPath());
             } catch (Exception e) {
+                //社員情報保存CSV＆社員情報リスト以外で例外が発生した場合　(メモリがいっぱいなど)
+                manager.printErrorLog(e, "社員情報の削除に失敗しました");
+                showErrorDialog("社員情報の削除に失敗しました");
+                return;
             } finally {
-
                 LOCK.unlock();
             }
         }
