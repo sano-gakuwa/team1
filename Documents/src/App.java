86d63f0a5459d6baf04836b0a2a90ac34020179b
ki610 <tool.kcmw@gmail.com>
2025-06-10 23:48:24 +0900

    Update App.java
    不要なコメントアウトを消しました。

--- Documents/src/App.java
+++ Documents/src/App.java
@@ -1,10 +1,7 @@
 public class App {
 
     public static void main(String[] args) throws Exception {
-        //↓本来のコード、999人テスト時はコメントアウトしてください（木下）
         EmployeeManager manager = new EmployeeManager();
-        //↓★999人テスト用ダミーコード↓
-        //TestEmployeeManager manager = new TestEmployeeManager(); //テスト時のみ使用、999人登録コード
         manager.setUp();
     }
 }
