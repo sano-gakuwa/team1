e6b610c991b83555dcd1a4d8abf3aa31b32a3a24
Simomura <simomura3109@gmail.com>
2025-06-22 02:54:53 +0900

    EmployeeInformationの仕様変更による変更

--- Documents/src/EmployeeInformation.java
+++ Documents/src/EmployeeInformation.java
@@ -1,49 +1,163 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class EmployeeInformation {
-    String employeeID;
-    String lastName;
-    String firstname;
-    String rubyLastName;
-    String rubyFirstname;
-    Date birthday;
-    Date joiningDate;
-    int engineerDate;
-    String availableLanguages;
-    String careerDate;
-    String trainingDate;
-    Double skillPoint;
-    Double attitudePoint;
-    Double communicationPoint;
-    Double leadershipPoint;
-    String remarks;
-    Date updatedDay;
+    private String employeeID;
+    private String lastName;
+    private String firstname;
+    private String rubyLastName;
+    private String rubyFirstname;
+    private Date birthday;
+    private Date joiningDate;
+    private int engineerDate;
+    private String availableLanguages;
+    private String careerDate;
+    private String trainingDate;
+    private Double skillPoint;
+    private Double attitudePoint;
+    private Double communicationPoint;
+    private Double leadershipPoint;
+    private String remarks;
+    private Date updatedDay;
 
-    public EmployeeInformation(
-            String employeeID,
-            String lastName, String firstname,
-            String rubyLastName, String rubyFirstname,
-            Date birthday, Date joiningDate, int engineerDate,
-            String availableLanguages, String careerDate, String trainingDate,
-            Double skillPoint, Double attitudePoint, Double communicationPoint, Double leadershipPoint,
-            String remarks, Date updatedDay) {
+    public EmployeeInformation() {
+
+    }
+
+    public void setEmployeeID(String employeeID) {
         this.employeeID = employeeID;
+    }
+
+    public void setlastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public void setFirstname(String firstname) {
         this.firstname = firstname;
+    }
+
+    public void setRubyLastName(String rubyLastName) {
         this.rubyLastName = rubyLastName;
+    }
+
+    public void setRubyFirstname(String rubyFirstname) {
         this.rubyFirstname = rubyFirstname;
+    }
+
+    public void setBirthday(Date birthday) {
         this.birthday = birthday;
+    }
+
+    public void setJoiningDate(Date joiningDate) {
         this.joiningDate = joiningDate;
+    }
+
+    public void setEngineerDate(int engineerDate) {
         this.engineerDate = engineerDate;
+    }
+
+    public void setAvailableLanguages(String availableLanguages) {
         this.availableLanguages = availableLanguages;
+    }
+
+    public void setCareerDate(String careerDate) {
         this.careerDate = careerDate;
+    }
+
+    public void setTrainingDate(String trainingDate) {
         this.trainingDate = trainingDate;
+    }
+
+    public void setSkillPoint(Double skillPoint) {
         this.skillPoint = skillPoint;
+    }
+
+    public void setAttitudePoint(Double attitudePoint) {
         this.attitudePoint = attitudePoint;
+    }
+
+    public void setCommunicationPoint(Double communicationPoint) {
         this.communicationPoint = communicationPoint;
+    }
+
+    public void setLeadershipPoint(Double leadershipPoint) {
         this.leadershipPoint = leadershipPoint;
+    }
+
+    public void setRemarks(String remarks) {
         this.remarks = remarks;
+    }
+
+    public void setUpdatedDay(Date updatedDay) {
         this.updatedDay = updatedDay;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getRubyLastName() {
+        return rubyLastName;
+    }
+
+    public String getRubyFirstname() {
+        return rubyFirstname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public Date getJoiningDate() {
+        return joiningDate;
+    }
+
+    public int getEngineerDate() {
+        return engineerDate;
+    }
+
+    public String getAvailableLanguages() {
+        return availableLanguages;
+    }
+
+    public String getCareerDate() {
+        return careerDate;
+    }
+
+    public String getTrainingDate() {
+        return trainingDate;
+    }
+
+    public Double getSkillPoint() {
+        return skillPoint;
+    }
+
+    public Double getAttitudePoint() {
+        return attitudePoint;
+    }
+
+    public Double getCommunicationPoint() {
+        return communicationPoint;
+    }
+
+    public Double getLeadershipPoint() {
+        return leadershipPoint;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public Date getUpdatedDay() {
+        return updatedDay;
     }
 
     /**
@@ -58,7 +172,7 @@ public class EmployeeInformation {
     public static String formatDate(Date date) {
         if (date == null)
             return "";
-        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy年MM月dd日");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
         return sdf.format(date);
     }
 }
