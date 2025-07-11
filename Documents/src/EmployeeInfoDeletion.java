b03425e3c110ce005817d9cd8d94f8adeb8120e1
Simomura0716 <simomura3109@gmail.com>
2025-07-11 20:10:05 +0900

    ThreadsManagerの実装

--- Documents/src/EmployeeInfoDeletion.java
+++ Documents/src/EmployeeInfoDeletion.java
@@ -16,7 +16,9 @@ import javax.swing.JOptionPane;
 
 public class EmployeeInfoDeletion implements Runnable {
     private ArrayList<String> selected;
+    private ArrayList<EmployeeInformation> backupEmployeeList;
     private final EmployeeManager MANAGER = new EmployeeManager();
+    private ThreadsManager threadsManager=new ThreadsManager();
     private static ReentrantLock deletionLock = new ReentrantLock();
 
     /**
@@ -35,87 +37,23 @@ public class EmployeeInfoDeletion implements Runnable {
     public void run() {
         deletionLock.lock();
         MANAGER.printInfoLog("社員情報削除の開始");
+        threadsManager.startUsing(Thread.currentThread());
         // 削除処理
         try {
             // 社員情報リストから選択した社員情報を削除
-            ArrayList<EmployeeInformation> backupEmployeeList;
             backupEmployeeList = EmployeeManager.employeeList;
-            synchronized (EmployeeManager.employeeList) {
-                try {
-                    for (Iterator<EmployeeInformation> employeeIterator = EmployeeManager.employeeList
-                            .iterator(); employeeIterator.hasNext();) {
-                        EmployeeInformation employee = employeeIterator.next();
-                        // 選択された社員情報と合致したら削除
-                        if (selected.contains(employee.getEmployeeID()) == true) {
-                            employeeIterator.remove();
-                        }
-                    }
-                } catch (Exception e) {
-                    MANAGER.printExceptionLog(e, "選択された社員情報の削除に失敗しました");
-                    // employeeListにbackupEmployeeListをコピー
-                    EmployeeManager.employeeList.clear();
-                    EmployeeManager.employeeList = backupEmployeeList;
-                    MANAGER.printInfoLog("社員情報リストを削除処理前に戻しました");
-                    showErrorDialog("選択された社員情報の削除に失敗しました");
-                    return;
-                }
-            }
+            deletingOfList();
             // 社員情報保存CSVから選択した社員情報を削除
             File originalFile = EmployeeManager.EMPLOYEE_CSV;
             File backupFile = new File("CSV/employee_data_backup.csv");
             FileLock lock = null;
             try {
-                try {
-                    // バックアップファイル作成
-                    Files.copy(originalFile.toPath(), backupFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                } catch (IOException e) {
-                    MANAGER.printExceptionLog(e, "バックアップファイルの作成に失敗しました");
-                    showErrorDialog("バックアップファイルの作成に失敗しました");
-                    return;
-                }
-                FileOutputStream fileOutputStream = new FileOutputStream(originalFile);
-                FileChannel channel = fileOutputStream.getChannel();
-                lock = channel.lock(); // CSVファイルの排他ロック（同時書き込み防止）
-                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "Shift-JIS");
-                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
-                PrintWriter printWriter = new PrintWriter(bufferedWriter);
-                // 社員情報保存CSVファイルの1行目に項目名を記載
-                printWriter.println(String.join(",", EmployeeManager.EMPLOYEE_CATEGORY));
-                // 社員情報リストの内容を社員情報保存CSVに上書き保存
-                for (EmployeeInformation employee : EmployeeManager.employeeList) {
-                    printWriter.println(MANAGER.convertToCSV(employee));
-                }
-                printWriter.close();
-                try {
-                    if (lock != null && lock.isValid()) {
-                        lock.release(); // エラーでもロック解除
-                        MANAGER.printInfoLog("社員情報保存CSVファイルロック解除成功");
-                    }
-                } catch (IOException ex) {
-                    MANAGER.printInfoLog("社員情報保存CSVファイルのロック解除失敗");
-                }
+                createbackupFile(originalFile, backupFile);
+                csvWriting(originalFile,lock);
             } catch (Exception e) {
-                // 追記中エラー時のロールバック処理を追加
-                try {
-                    if (lock != null && lock.isValid()) {
-                        lock.release(); // エラーでもロック解除
-                        MANAGER.printInfoLog("社員情報保存CSVファイルロック解除成功");
-                    }
-                } catch (IOException ex) {
-                    MANAGER.printInfoLog("社員情報保存CSVファイルロック解除失敗");
-                }
+                tryUnLock(lock);
                 MANAGER.printExceptionLog(e, "削除後の社員情報リストの保存に失敗しました");
-                if (originalFile.exists() && backupFile.exists()) {
-                    try {
-                        Files.deleteIfExists(originalFile.toPath());
-                    } catch (Exception ex) {
-                        // オリジナルの社員情報保存CSVファイルが削除に失敗した場合
-                        MANAGER.printExceptionLog(ex, "オリジナルの社員情報保存CSVファイルが削除できませんでした");
-                        showErrorDialog("オリジナルの社員情報保存CSVファイルが削除できませんでした");
-                        return;
-                    }
-                    backupFile.renameTo(originalFile);
-                }
+                revertingOriginalFile(originalFile, backupFile);
                 MANAGER.printInfoLog("社員情報保存CSVファイルを削除処理前に戻しました");
                 showErrorDialog("削除後の社員情報リストの保存に失敗しました");
                 return;
@@ -126,9 +64,102 @@ public class EmployeeInfoDeletion implements Runnable {
             MANAGER.printExceptionLog(e, "社員情報の削除に失敗しました");
             showErrorDialog("社員情報の削除に失敗しました");
             return;
+        }finally{
+            deletionLock.unlock();
+            threadsManager.endUsing(Thread.currentThread());
         }
-        deletionLock.unlock();
         MANAGER.printInfoLog("社員情報削除の完了");
+    }
+
+    /**
+     * 
+     */
+    private void deletingOfList() {
+        synchronized (EmployeeManager.employeeList) {
+            try {
+                for (Iterator<EmployeeInformation> employeeIterator = EmployeeManager.employeeList
+                        .iterator(); employeeIterator.hasNext();) {
+                    EmployeeInformation employee = employeeIterator.next();
+                    // 選択された社員情報と合致したら削除
+                    if (selected.contains(employee.getEmployeeID()) == true) {
+                        employeeIterator.remove();
+                    }
+                }
+            } catch (Exception e) {
+                MANAGER.printExceptionLog(e, "選択された社員情報の削除に失敗しました");
+                // employeeListにbackupEmployeeListをコピー
+                EmployeeManager.employeeList.clear();
+                EmployeeManager.employeeList = backupEmployeeList;
+                MANAGER.printInfoLog("社員情報リストを削除処理前に戻しました");
+                showErrorDialog("選択された社員情報の削除に失敗しました");
+                return;
+            }
+        }
+    }
+
+    private void createbackupFile(File originalFile, File backupFile) {
+        try {
+            // バックアップファイル作成
+            Files.copy(originalFile.toPath(), backupFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            MANAGER.printExceptionLog(e, "バックアップファイルの作成に失敗しました");
+            showErrorDialog("バックアップファイルの作成に失敗しました");
+            return;
+        }
+    }
+
+    private void csvWriting(File originalFile, FileLock lock) {
+        try (PrintWriter printWriter = new PrintWriter(setBufferedWriter(originalFile, lock));) {
+            // 社員情報保存CSVファイルの1行目に項目名を記載
+            printWriter.println(String.join(",", EmployeeManager.EMPLOYEE_CATEGORY));
+            // 社員情報リストの内容を社員情報保存CSVに上書き保存
+            for (EmployeeInformation employee : EmployeeManager.employeeList) {
+                printWriter.println(MANAGER.convertToCSV(employee));
+            }
+        } catch (Exception e) {
+            MANAGER.printExceptionLog(e, "社員情報保存CSVファイルに書き込み失敗");
+        } finally {
+            tryUnLock(lock);
+        }
+    }
+
+    private BufferedWriter setBufferedWriter(File originalFile, FileLock lock) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(originalFile);
+            FileChannel channel = fileOutputStream.getChannel();
+            lock = channel.lock(); // CSVファイルの排他ロック（同時書き込み防止）
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream, "Shift-JIS");
+            bufferedWriter = new BufferedWriter(outputStreamWriter);
+        } catch (Exception e) {
+            MANAGER.printExceptionLog(e, "BufferedWriterの設定に失敗");
+        }
+        return bufferedWriter;
+    }
+
+    private void tryUnLock(FileLock lock) {
+        try {
+            if (lock != null && lock.isValid()) {
+                lock.release(); // エラーでもロック解除
+                MANAGER.printInfoLog("社員情報保存CSVファイルロック解除成功");
+            }
+        } catch (IOException e) {
+            MANAGER.printExceptionLog(e, "社員情報保存CSVファイルのロック解除失敗");
+        }
+    }
+
+    private void revertingOriginalFile(File originalFile, File backupFile) {
+        if (originalFile.exists() && backupFile.exists()) {
+            try {
+                Files.deleteIfExists(originalFile.toPath());
+            } catch (Exception e) {
+                // オリジナルの社員情報保存CSVファイルが削除に失敗した場合
+                MANAGER.printExceptionLog(e, "オリジナルの社員情報保存CSVファイルが削除できませんでした");
+                showErrorDialog("オリジナルの社員情報保存CSVファイルが削除できませんでした");
+                return;
+            }
+            backupFile.renameTo(originalFile);
+        }
     }
 
     /**
